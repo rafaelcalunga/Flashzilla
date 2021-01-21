ee505486b0277872459eba1454da6896c264b3f0
Rafael Calunga <rafaelcalunga@gmail.com>
2021-01-20 23:19:11 -0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Rafael Calunga on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
