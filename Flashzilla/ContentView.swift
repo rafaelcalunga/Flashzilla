2f27195d4d8f647780ffce8a60699e8fea855312
Rafael Calunga <rafaelcalunga@gmail.com>
2021-02-01 13:57:44 -0500

    Counting down with a Timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -19,6 +19,10 @@ struct ContentView: View {
     
     @State private var cards = [Card](repeating: Card.example, count: 10)
     
+    @State private var isActive = true
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -27,6 +31,17 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
+                Text("Time \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.75)
+                    )
+                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -59,6 +74,19 @@ struct ContentView: View {
                     .padding()
                 }
             }
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            self.isActive = true
         }
     }
     
